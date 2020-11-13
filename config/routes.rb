5ef6dd1b4748e12c7238b19098fa5f01a0d4cddd
SidAbhi <megamancodenamezerobytes@gmail.com>
2020-11-13 20:31:26 +0700

    Updated gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
-  get 'users/index'
+  get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get 'pages/index'
   #get 'page/index'
-  root 'users#index'
+  root 'pages#index'
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :new]
+      resources :posts
     end
   end
 
-  # get '*path', to: 'pages#index', via: :all
+  get '*path', to: 'pages#index', via: :all
 end
