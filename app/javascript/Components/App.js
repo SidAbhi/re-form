3e891676de3ba2d426f94dabe7e5f828a9bc1e55
SidAbhi <megamancodenamezerobytes@gmail.com>
2020-11-13 18:44:59 +0700

    Scaffold twitter clone

---
+++ app/javascript/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter as Route, Switch, Router} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className="main">
+      Test
+    </div>
+  );
+};
+
+export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :posts
+  get 'users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get 'pages/index'
+  #get 'page/index'
+  root 'users#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :new]
+    end
+  end
+
+  # get '*path', to: 'pages#index', via: :all
 end
